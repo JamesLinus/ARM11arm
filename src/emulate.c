17449c301ea8bcf4fc3eff867427e504cc63f61a
Alan Vey <amv12@ic.ac.uk>
2013-05-30 06:46:27 +0100

    Implements basic skeleton (really basic)

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,16 @@
 #include <stdlib.h>
 
+void loadBinaryFile(char* path){
+	FILE *file;
+
+	file = fopen(path, "b");
+	if (file) {
+    	fread(buffer, MAX_FILE_SIZE, 1, file);
+	}
+	else {
+  	  // file opneing error
+	}
+
 int main(int argc, char **argv) {
   printf(argc);
   return EXIT_SUCCESS;
