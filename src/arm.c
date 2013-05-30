dd5d08b238f46f8a8eeb6f0aa37bae7043ccceea
Alan Vey <amv12@ic.ac.uk>
2013-05-30 23:20:49 +0100

    Set up state for emulator & print regs when done

--- src/arm.c
+++ src/arm.c
@@ -8,9 +8,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <stdint.h>
 
 void initialize(void)
 {
+  struct state
+  {
+    uint32_t memory[2048];
+    uint32_t registor[17];
+  };
+
+  struct state st = { 0 };
+  const int NO_OF_REGS = 17;
+
+  while(NO_OF_REGS == 17) //use pipeline to execte wrong terminate cond
+  {
+
+  }
+
+  for(int n = 0; n < NO_OF_REGS; n++)
+  {
+    printf("Registor %d: %d/n", n, st.registor[n]);
+  }
 }
