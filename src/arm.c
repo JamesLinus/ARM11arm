3823be185903b7b005ac3663b08c7f99f33e5ef3
Alan Vey <amv12@ic.ac.uk>
2013-05-31 03:37:27 +0100

    Attempt at readind into emulator memory

--- src/arm.c
+++ src/arm.c
@@ -9,19 +9,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "instructionSet.h"
+#include "utilities.h"
 
 uint32_t decodeInstruction(uint32_t loadInst, uint32_t *memory, uint32_t *registor)
 {
   return 0;
 }
 
-void initialize(void)
+void initialize(char *path)
 {
+  const int NO_OF_REGS = 17;
+  const int WORDS_IN_MEMORY = 16384;
+
   // set up state of the emulator
   struct EmulatorState
   {
-    uint32_t memory[16384];
-    uint32_t registor[17];
+    uint32_t memory[WORDS_IN_MEMORY];
+    uint32_t registor[NO_OF_REGS];
   };
 
   // set up the pipeline state
@@ -32,13 +37,15 @@ void initialize(void)
   };
 
   // initialise all struct elements to 0 
-  struct EmulatorState eState = { 0 };
-  struct PipelineState pState = { 0 };
-  const int NO_OF_REGS = 17;
+  struct EmulatorState eState = { (uint32_t) 0 };
+  struct PipelineState pState = { (uint32_t) 0 };
+
+  // load bin file into memory
+  eState.memory = loadBinaryFile(path);
 
   while(NO_OF_REGS == 17)// note change condition
   {
-    // decode the loaded intruction
+    // decode the loaded instruction and execute
     pState.decodeInst = decodeInstruction(pState.loadInst, eState.memory, eState.registor);
     // load the instruction from memory
     pState.loadInst = eState.memory[eState.registor[15]];
