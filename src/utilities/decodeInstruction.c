d8470f4c531e9b33b42804384614cd61ee3427ad
Alan Vey <amv12@ic.ac.uk>
2013-06-04 07:34:32 +0100

    Decode for Data Processing done + auxiliary function also used in data transfer

--- src/utilities/decodeInstruction.c
+++ src/utilities/decodeInstruction.c
@@ -9,8 +9,30 @@
 BaseOpInstr* decodeInstruction(Arm* raspi, u32 index)
 {
   u32 instr = raspi->em[index];
+  BaseInstr* base = raspi->dm[index];
+  // cond is the same no matter the instruction
+  base.cond = instr >> 28;
+
   if (IS_DATA(instr))
   { // opcode matches data processing
+    DataProcessingInstr* i = (DataProcessingInstr*) &base;
+    i.opcode = instr & DATA_OP_MASK;
+    switch(i.opcode)
+    {
+      case 0: i.function = 0; break; // add funciton pointer
+      case 1: i.function = 0; break; // add funciton pointer
+      case 2: i.function = 0; break; // add funciton pointer
+      case 3: i.function = 0; break; // add funciton pointer
+      case 4: i.function = 0; break; // add funciton pointer
+      case 8: i.function = 0; break; // add funciton pointer
+      case 9: i.function = 0; break; // add funciton pointer
+      case 10: i.function = 0; break; // add funciton pointer
+      case 12: i.function = 0; break; // add funciton pointer
+      case 13: i.function = 0; break; // add funciton pointer
+    }
+    i.op1 = raspi.r[instr & RN_MASK];
+    i.des = raspi.r[instr & RD_MASK];
+    i.op2 = imediateOrReg(instr);
 
   }
   else if (IS_MUL(instr))
@@ -31,3 +53,43 @@ BaseOpInstr* decodeInstruction(Arm* raspi, u32 index)
   }
   return (BaseOpInstr*) &(raspi->dm[index]);
 }
+
+u32* imediateOrReg(u32 instr)
+{
+  u32 opr2 = instr & DATA_OPR_2;
+  if(instr & IMMEDIATE_MASK)
+  {
+    // only shifted 7 due to multiply by 2
+    return &ROR((instr & DATA_OP2_ROTATE) >> 7, opr2 & DATA_OP2_IMM)
+  } else
+  {
+    u32 shift;
+    u32 opr2 = instr & RM_MASK;
+
+    if(instr & DATA_OP2_CTRL)
+    {
+      // & has precedence so no brackets
+      shift = raspi->r[instr & RS_MASK >> 8];
+    } else
+    {
+      shift = instr & DATA_OP2_INT >> 7
+    }
+    switch(instr & DATA_OP2_SHIFT >> 5)
+    {
+      case 1: return opr2 << shift;
+      case 2: return opr2 >> shift;
+      case 3: return ASR(shift, opr2);
+      case 4: return ROR(shift, opr2);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
