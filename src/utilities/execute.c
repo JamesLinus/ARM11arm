2c24900fa189c32e9c998280aa2618a3db779812
Lawrence Jones <lawrence.jones12@imperial.ac.uk>
2013-06-09 05:09:09 +0100

    Refactoring execute functions, done SDTrnsfr and branch

--- src/utilities/execute.c
+++ src/utilities/execute.c
@@ -9,84 +9,60 @@
 #include "execute.h"
 #include <stdio.h>
 
-
+// Execute function
+// Offsets the PC for the raspi
 void branch(PtrToBeCast base)
 {
+  // make the casting to correct struct
   BranchInstr *i = (BranchInstr *) base;
-  if (checkFlags(i->cpsr, i->cond))
+  // if the offset is to add
+  if (i->toAdd)
   {
-    if (i->toAdd)
-    {
-      i->pc += i->offset;
-    }
-    else
-    {
-      i->pc -= i->offset;
-    }
+    // then increment the pc
+    i->pc += i->offset;
+    return;
   }
+  // else decrement
+  i->pc -= i->offset;
 }
 
+// Execute function
+// Deals with single data transfer
 void singleDataTransfer(PtrToBeCast base)
 {
+  // make the appropriate casting
   SingleDataInstr *i = (SingleDataInstr *) base;
-
-  if (checkFlags(i->cpsr, i->cond))
-  {
-    *(i->op2) = (*(i->exShift))(i->cpsr, *(i->op2), *(i->shift));
-
-    if (i->p)
-    {
-      if (i->u)
-      {
-        i->op1 += *(i->op2);
-      }
-      else
-      {
-        i->op1 -= *(i->op2);
-      }
-      if (i->l)
-      {
-        *(i->des) = *(i->op2);
-      }
-      else
-      {
-        *(i->op2) = *(i->des);
-      }
-    }
-    else
-    {
-      if (i->l)
-      {
-        *(i->des) = *(i->op2);
-      }
-      else
-      {
-        *(i->op2) = *(i->des);
-      }
-      if (i->u)
-      {
-        i->op1 += *(i->op2);
-      }
-      else
-      {
-        i->op1 -= *(i->op2);
-      }
-    }
+  // find the op2 value from appropriate shifting
+  *(i->op2) = (*(i->exShift))(i->cpsr, *(i->op2), *(i->shift));
+  // generate unique code for each modifier
+  // TODO - stop using separate vars for the p u & l
+  int code = (i->p << 2) + (i->u << 1) + i->l;
+  // switch for the op1 modifiers
+  switch(code) {
+    case 0x02u: case 0x03u:
+    case 0x06u: case 0x07u: i->op1   += *(i->op2);
+    case 0x04u: case 0x05u:
+    case 0x00u: case 0x01u: i->op1   -= *(i->op2);
+  } 
+  // switch for the des modifiers
+  switch(code) {
+    case 0x01u: case 0x03u:
+    case 0x05u: case 0x07u: *(i->des) = *(i->op2);
+    case 0x00u: case 0x02u:
+    case 0x04u: case 0x06u: *(i->op2) = *(i->des);
   }
 }
 
+// Execute function
+// Deals with the multiplication, ideally with 2s complement
 void multiply(PtrToBeCast base)
 {
+  // cast to appropriate struct type
   MultiplyInstr* i = (MultiplyInstr*) base;
-
-  if (checkFlags(i->cpsr, i->cond))
-  {
-    *(i->des) = ((*(i->op1)) * (*(i->op2))) + (*(i->acc));
-    if(i->s)
-    {
-      setflags(i->cpsr, *(i->des));
-    } 
-  }
+  // make calculation, move into destination
+  *(i->des) = ((*(i->op1)) * (*(i->op2))) + (*(i->acc));
+  // if required, mark the flags
+  if (i->s) setflags(i->cpsr, *(i->des));
 }
 
 // inefficent a lot of repetition will find a better solution later
