8402c99903a386665adf9b33bc6f12795c9aa87c
Sean Naderi <sean.naderi12@imperial.ac.uk>
2013-06-11 23:54:55 +0100

    header for assembling instructions

---
+++ src/utilities/functionAssemble.h
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+// C Group Project - First Year
+// ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+// File: functionAssemble.h
+// Group: 21
+// Members: amv12, lmj112, skd212
+///////////////////////////////////////////////////////////////////////////////
+
+#include<stdint.h>
+
+uint32_t assembleDataProcessing();
+
+uint32_t assembleMultiply();
+
+uint32_t assembleSingleDataTransfer();
+
+uint32_t assembleBranch();
