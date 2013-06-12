b1bf105f9e596b371432aa4253e492b9133a6912
Sean Naderi <sean.naderi12@imperial.ac.uk>
2013-06-12 01:01:23 +0100

    opcodeBits filled in

--- src/utilities/functionAssemble.c
+++ src/utilities/functionAssemble.c
@@ -9,6 +9,8 @@
 #include "functionAssemble.h"
 #include <assert.h>
 
+//to think about ==== what to do about the labels
+
 uint32_t assembleDataProcessing(int arguments char **strings)
 {
   assert (arguments >=2 && arguments <=5);
@@ -21,7 +23,59 @@ uint32_t assembleDataProcessing(int arguments char **strings)
   uint32_t bits26and27mask 0xF3FFFFFF;
   //update to include bits 26 and 27
   binaryCode = binaryCode & bits26and27mask;
+  //TODO set bit 25 - the I bit
   
+  //for bits 21 through 24 - opcode
+  uint32_t opCodemask;  
+
+  if(!strcmp(strings[0], "and")
+  { 
+     opCodeMask = 0xFE1FFFFF;
+  }
+  else if(!strcmp(strings[0], "eor")
+  { 
+     opCodeMask = 0xFE3FFFFF;
+  }
+  else if(!strcmp(strings[0], "sub")
+  { 
+     opCodeMask = 0xFE5FFFFF;
+  }
+  else if(!strcmp(strings[0], "rsb")
+  { 
+     opCodeMask = 0xFE7FFFFF;
+  }
+  else if(!strcmp(strings[0], "add")
+  { 
+     opCodeMask = 0xFE9FFFFF;
+  }
+  else if(!strcmp(strings[0], "orr")
+  { 
+     opCodeMask = 0xFF9FFFFF;
+  }  
+  else if(!strcmp(strings[0], "mov")
+  { 
+     opCodeMask = 0xFFBFFFFF;
+  } 
+  else if(!strcmp(strings[0], "tst")
+  { 
+     opCodeMask = 0xFF1FFFFF;
+  }
+  else if(!strcmp(strings[0], "teq")
+  { 
+     opCodeMask = 0xFF3FFFFF;
+  }
+  else if(!strcmp(strings[0], "cmp")
+  { 
+     opCodeMask = 0xFF5FFFFF;
+  }
+  else
+    //isnt data processing!
+    printf("this isnt a data processing instruction!\n");
+  
+  //now we have the correct opCodeMask we can fill in those bits in the 
+  //binaryCode
+  binaryCode = binaryCode & opCodeMask;
+
   switch(arguments)
   {
     case 2:
